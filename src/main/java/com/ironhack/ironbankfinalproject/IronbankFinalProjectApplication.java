515602c4796161e75920e139f2ccc1f3cde28aba
sjacas <sergi.jacas@wezaam.com>
2022-09-04 13:36:26 +0200

    Initialize project

---
+++ src/main/java/com/ironhack/ironbankfinalproject/IronbankFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.ironbankfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronbankFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IronbankFinalProjectApplication.class, args);
+    }
+
+}
