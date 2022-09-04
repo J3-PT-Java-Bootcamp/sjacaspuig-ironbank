5c418c915cb5779e5f87586fac86fec19ab0d6ce
sjacas <sergi.jacas@wezaam.com>
2022-09-04 13:40:11 +0200

    Change project name

--- src/main/java/com/ironhack/ironbank/IronbankFinalProjectApplication.java
+++ src/main/java/com/ironhack/ironbank/IronbankFinalProjectApplication.java
@@ -1,4 +1,4 @@
-package com.ironhack.ironbankfinalproject;
+package com.ironhack.ironbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ironhack/ironbank/IronbankFinalProjectApplicationTests.java
+++ src/test/java/com/ironhack/ironbank/IronbankFinalProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ironhack.ironbankfinalproject;
+package com.ironhack.ironbank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
